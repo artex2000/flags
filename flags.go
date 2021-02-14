673624c5873c5f5198b427469c2a56e79f92c77a
Artem Shchygel <artems@ami.com>
2021-02-13 20:52:23 -0500

    imp: Add more features

--- flags.go
+++ flags.go
@@ -2,13 +2,39 @@ package main
 
 import (
         "os"
+        "io"
         "fmt"
         "log"
         "bufio"
+        "bytes"
         "strings"
+        "flag"
+        "sort"
         "regexp"
         "path/filepath"
 )
+
+var (
+        target      string
+        toolchain   string
+        arch        string
+        command     string
+        attribute   string
+    )
+
+
+func init () {
+    flag.StringVar(&target,    "t", "*", "set target")
+    flag.StringVar(&toolchain, "n", "*", "set toolchain")
+    flag.StringVar(&arch,      "a", "*", "set arch")
+    flag.StringVar(&command,   "c", "*", "set command")
+    flag.StringVar(&attribute, "r", "*", "set attribute")
+}
+
+func Usage() {
+        fmt.Printf("Usage:\n\tflags.exe [flags] filename\n")
+        flag.PrintDefaults()
+}
 
 type Property struct {
         Target    string
@@ -25,15 +51,13 @@ type Define struct {
 }
 
 func (p Property) IsMatch(t Property) bool {
-        target := (t.Target == "") || (t.Target == "*") ||
-                  (p.Target == "*") || (p.Target == t.Target)
-        toolchain := (t.Toolchain == "") || (t.Toolchain == "*") ||
-                  (p.Toolchain == "*") || (p.Toolchain == t.Toolchain)
-        arch := (t.Arch == "") || (t.Arch == "*") ||
-                  (p.Arch == "*") || (p.Arch == t.Arch)
-        comm := (t.Command == "") || (t.Command == "*") ||
-                  (p.Command == "*") || (p.Command == t.Command)
-        return target && toolchain && arch && comm
+        target    := (t.Target == "*")    || (p.Target == t.Target)
+        toolchain := (t.Toolchain == "*") || (p.Toolchain == t.Toolchain)
+        arch      := (t.Arch == "*")      || (p.Arch == t.Arch)
+        comm      := (t.Command == "*")   || (p.Command == t.Command)
+        attr      := (t.Attribute == "*") || (p.Attribute == t.Attribute)
+
+        return target && toolchain && arch && comm && attr
 }
 
 func (p Property) FullName() string {
@@ -45,19 +69,21 @@ func (p Property) FullName() string {
 }
 
 func (p Property) IsStar() bool {
-        return (p.Target == "*" &&
+        return (p.Target    == "*" &&
                 p.Toolchain == "*" &&
-                p.Arch == "*" &&
-                p.Command == "*")
+                p.Arch      == "*" &&
+                p.Command   == "*")
 }
 
 func main() {
-        if len(os.Args) < 2 {
-                fmt.Printf("Usage: %v filename\n", filepath.Base(os.Args[0]))
+        flag.Parse()
+
+        if flag.NArg() == 0 {
+                Usage()
                 os.Exit(1)
         }
 
-        file := os.Args[1]
+        file := flag.Arg(0)
         if ok := filepath.IsAbs(file); !ok {
                 var err error
                 file, err = filepath.Abs(file)
@@ -101,16 +127,145 @@ func main() {
         fatal(err)
         fmt.Printf("Success, %d records\n", len(prop))
 
-        tmp := Property{ Toolchain : "GCC5", Arch : "AARCH64", Command : "DLINK" }
-        fil := filter(prop, tmp, false) 
+        err = export_flags(prop)
+        fatal(err)
+
+        /*
+        tmp := Property{ Target : target,
+                         Toolchain : toolchain, 
+                         Arch : arch, 
+                         Command : command,
+                         Attribute: attribute }
+
+        var buf bytes.Buffer
+        fil := filter(prop, tmp) 
         if len(fil) != 0 {
-                for _, v := range fil {
-                        fmt.Printf("%s = %s\n", v.FullName(), v.Value)
-                }
+                print_props(fil, true, &buf)
+                out := buf.String()
+                fmt.Println(out)
         } else {
                 fmt.Printf("No matches for %v\n", tmp)
         }
+        */
 }
+
+func export_flags(p []Property) error {
+        var out bytes.Buffer
+
+        tmp := Property { Target    : target,
+                          Toolchain : toolchain,
+                          Arch      : arch,
+                          Command   : command,
+                          Attribute : "FLAGS" }
+
+        command := [] string {
+                "CC",
+                "ASM",
+                "DLINK",
+                "DLINK2",
+                "PP",
+                "VFRPP",
+                "ASLPP",
+                "APP",
+                "ASLCC",
+                "ASLDLINK",
+                "RC",
+                "NASM",
+                "*",
+        }
+
+        arch := []string { "IA32", "X64", "AARCH64" }
+
+        cnt := 0
+        fl, _ := filter(p, tmp)
+        fmt.Printf("%d properties filtered\n", len(fl))
+
+        for _, a := range arch {
+                var r []Property
+                tmp.Arch = a
+                for _, c := range command {
+                        tmp.Command = c
+                        r, fl = filter(fl, tmp)
+                        cnt += len(r)
+                        print_props(r, true, &out)
+                }
+        }
+
+        fmt.Printf("%d properties outed\n", cnt)
+
+        var name string
+        if tmp.Toolchain != "*" {
+                name = tmp.Toolchain + "_flags.txt"
+        } else {
+                name = "AMI" + "_flags.txt"
+        }
+        fname, err := filepath.Abs(name)
+        if err != nil {
+                return err
+        }
+
+        f, err := os.Create(fname)
+        if err != nil {
+                return err
+        }
+        defer f.Close()
+
+        _, err = f.WriteString(out.String())
+        if err != nil {
+                return err
+        }
+        return nil
+}
+
+
+
+func print_props(p []Property, pretty bool, w io.Writer) {
+        var stream io.Writer
+        if w == nil {
+                stream = os.Stdout
+        } else {
+                stream = w
+        }
+
+        for _, v := range p {
+                fmt.Fprintf(stream, "%s = ", v.FullName())
+                if len(v.Value) < 50 || !pretty{
+                        fmt.Fprintf(stream, "%s\n", v.Value)
+                } else {
+                        fmt.Fprintf(stream, "\n")
+                        lst := split_flags(v.Value)
+                        for _, it := range lst {
+                                fmt.Fprintf(stream, "\t%s\n", it)
+                        }
+                }
+        }
+}
+
+func split_flags(f string) []string {
+        var r []string
+        //remove extra spaces between words
+        sp := regexp.MustCompile(`\s+`)
+        s := sp.ReplaceAllString(f, " ")
+
+        tmp := strings.Split(s, " ")
+        i := 1
+        for i <= len(tmp) {
+                a := tmp[i-1]
+                if i < len(tmp) {
+                        b := tmp[i]
+                        if b[0] != '-' && b[0] != '/' {
+                                a = a + " " + b
+                                i += 1
+                        }
+                }
+                r = append(r, a)
+                i += 1
+        }
+
+        sort.Strings(r)
+        return r
+}
+        
 
 func unwrap_defs(d []Define) (map[string]string, error) {
         var w []Define
@@ -184,17 +339,17 @@ func unwrap_prop(pr []Property, df map[string]string) ([]Property, error) {
         return rs, nil
 }
 
-func filter(pr []Property, t Property, all bool) []Property {
-        var rs []Property
+func filter(pr []Property, t Property) ([]Property, []Property) {
+        var need []Property
+        var rest []Property
         for _, v := range pr {
-                if !all && v.IsStar() {
-                        continue
-                }
                 if v.IsMatch(t) {
-                        rs = append(rs, v)
+                        need = append(need, v)
+                } else {
+                        rest = append(rest, v)
                 }
         }
-        return rs
+        return need, rest
 }
 
 func fatal(err error, msg ...string) {
